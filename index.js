2244cd67708c155c9c3068d5881c636b1077f08f
Pavel-Pashko <42612270+Pavel-Pashko@users.noreply.github.com>
2018-08-26 00:12:52 +1100

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,6 @@ const TOKEN = '666335587:AAFoSh5nfqoLJcdDkoEUDSf3y9s5P4U7bVM'
 
 const bot = new TelegramBot(TOKEN, {polling: true})
 
-bot.massege_handler(commands=['start'])
-
-def start(m):
-keyboard = types.RyplyKeyboardMarkup(resize_keyboard=True)
-keyboard.add(*[types.KeyboardButton(name)
-for name in ['Шерлок Холмс', 'Доктор Ватсон']])
-msg = bot.send_massage(m.chat.id, 'Кого выбираешь?',
-    reply_markup=keyboard)
-bot.register_next_step_handler(msg, name)
-
-bot.polling()
+bot.on('messege', msg => {
+    bot.sendMessage(msg.chat.Id, 'Hello. bot says: "Hi, $(msg.from.fist_name)"')
+})
